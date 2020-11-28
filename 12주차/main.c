6e05bbbc84fca3a5c35d82b51b22331ae729bbf6
Oh <clover7even@naver.com>
2020-11-28 23:16:53 +0900

    메모리에 저장

--- 12주차/main.c
+++ 12주차/main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
-
 int parseInt(char* c) {
     int decimal = 0, pos = 0;
     for (int i = strlen(c) - 1; i>=0 ; i--) {
@@ -19,10 +18,15 @@ int parseInt(char* c) {
 }
 
 int main(int argc, char* argv[]) {
+    char* memory[32759];
     char CODES[60];
     char *c;
-    int startADR, len;
+    int startADR, size, len, location;
     srand((unsigned int)time(NULL));
+    for (int i = 0; i < 32759; i++) {
+        memory[i] = malloc(sizeof(char)*2);
+        // memory[i] = "00";
+    }
     if (argv[1] == NULL) {
         printf("error: Command line argument is not entered.\n");
         return 0;
@@ -35,11 +39,44 @@ int main(int argc, char* argv[]) {
     }
     c = fgets(CODES, 16, fp);
     c = fgets(CODES, 5, fp);
-    len = parseInt(c);
+    size = parseInt(c);
+    size = 160;
 
-    do {
+    /* do {
         startADR = rand() % 32759;
-    } while (startADR + len > 32758);
-
+    } while (startADR + size > 32758); */
+    startADR = 2260;
+    location = startADR;
     printf("start address: %d\n", startADR);
+
+    while(!feof(fp)) {
+        if (feof(fp)) break;
+        char a = fgetc(fp);
+        if (a == 'T') {
+            int i = 0;
+            while (i < 6) {
+                a = fgetc(fp);
+                i++;
+            }
+            c = fgets(CODES, 3, fp);
+            len = parseInt(c) * 2;
+            printf("%d ", len);
+            c = fgets(CODES, len+1, fp);
+            printf("%s\n", CODES);
+            for (int i = 0; i < sizeof(CODES); i+=2) {
+                if(CODES[i] == 0) {
+                    break;
+                }
+                else { 
+                    printf("%c%c\n", CODES[i], CODES[i+1]);
+                    memory[location][0] = CODES[i];
+                    memory[location][1] = CODES[i+1];
+                    location++;
+                }
+            }
+        }
+    }
+    for (int i = startADR; i < startADR + size; i++) {
+        printf("%d: %s\n", i, memory[i]);
+    }
 }
